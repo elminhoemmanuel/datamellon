ac4e62a0ac10209b36a4cba63eeb91f5a02e691f
elminhoemmanuel <amodue15@gmail.com>
2021-10-25 11:56:38 -0700

    initial setup

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import React, { useState, useEffect } from 'react'
+
+
+export default function Home() {
+
+    return (
+        <>
+            <Head>
+                <title>SFS Capital</title>
+                <meta name="keywords" content="SFS Capital" />
+            </Head>
+
+            <div className=''>
+                datamellon
+            </div>
+        </>
+    )
+}
